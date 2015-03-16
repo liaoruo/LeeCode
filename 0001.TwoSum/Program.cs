30b3a9fdb8b402215a4f3dc13c31fd84fb79fee5
liaoruo <chenxin06122@gmail.com>
2015-03-16 12:27:22 +0800

    Problem0001

---
+++ 0001.TwoSum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0001.TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var ret = TwoSum(numbers, target);
+            
+            System.Console.WriteLine(ret == null ? "null" : ret.Item1 + "," + ret.Item2);
+        }
+        static public Tuple<int, int> TwoSum(int[] numbers, int target)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            int cnt = numbers.Count();
+            for (int i = 0; i < cnt; i++)
+            {
+                int need = target - numbers[i];
+                if (dic.ContainsKey(need))
+                {
+                    return new Tuple<int, int>(dic[need] + 1, i + 1);
+                }
+                else
+                {
+                    if (!dic.ContainsKey(numbers[i]))
+                    {
+                        dic.Add(numbers[i], i);
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
