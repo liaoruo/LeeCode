c041b82baf52595fe50c01efea2057db8b469d59
liaoruo <chenxin.private@gmail.com>
2015-05-19 01:18:12 +0800

    Problem0023

--- 0022.GenerateParentheses/Program.cs
+++ 0022.GenerateParentheses/Program.cs
@@ -10,6 +10,12 @@ namespace _0022.GenerateParentheses
     {
         static void Main(string[] args)
         {
+            Solution solution = new Solution();
+            var ret = solution.GenerateParenthesis(10);
+            foreach (var line in ret)
+            {
+                System.Console.WriteLine(line);
+            }
         }
     }
     public class Solution
@@ -19,8 +25,26 @@ namespace _0022.GenerateParentheses
             List<string> ret = new List<string>();
             if (n == 0)
                 return ret;
+            SubFun("", n, n, ref ret);
+            return ret;
+        }
+        public void SubFun(string prefix, int i, int j, ref List<string> list)
+        {
+            if (i == 0 && j == 0)
+            {
+                list.Add(prefix);
+                return;
+            }
 
-            return null;
+            if (i > 0)
+            {
+                SubFun(prefix + "(", i - 1, j, ref list);
+            }
+            if (i < j)
+            {
+                SubFun(prefix + ")", i, j - 1, ref list);
+            }
+
         }
     }
 }
---
+++ 0023.MergekSortedLists/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0023.MergekSortedLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode l1 = new ListNode(0);
+            l1.next = new ListNode(0);
+            l1.next.next = new ListNode(0);
+
+            ListNode l2 = new ListNode(0);
+            l2.next = new ListNode(2);
+            l2.next.next = new ListNode(3);
+
+            ListNode l3 = new ListNode(1);
+            l3.next = new ListNode(2);
+            l3.next.next = new ListNode(3);
+
+            Solution solution = new Solution();
+            var ret=solution.MergeKLists(new ListNode[] { l1, l2, l3 });
+            while(ret!=null)
+            {
+                System.Console.Write(ret.val + " ");
+                ret = ret.next;
+            }
+            System.Console.WriteLine();
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public class Solution
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            SortedDictionary<int, ListNode> sd = new SortedDictionary<int, ListNode>();
+            Dictionary<int, int> dupdic = new Dictionary<int, int>();
+
+            foreach (var list in lists)
+            {
+                var clone = list;
+                while (clone != null)
+                {
+                    if (dupdic.ContainsKey(clone.val))
+                    {
+                        dupdic[clone.val]++;
+                        clone = clone.next;
+                    }
+                    else
+                    {
+                        dupdic.Add(clone.val, 1);
+                        sd.Add(clone.val, clone);
+                        break;
+                    }
+                }
+            }
+
+            ListNode ret=new ListNode(int.MinValue);
+            ListNode temp=ret;
+            while (sd.Count != 0)
+            {
+                var min = sd.ElementAt(0).Value.val;
+
+                while(dupdic[min]>0)
+                {
+                    temp.next = new ListNode(min);
+                    temp = temp.next;
+                    dupdic[min]--;
+                }
+
+                var clone = sd.ElementAt(0).Value.next;
+                sd.Remove(min);
+                while (clone != null)
+                {
+                    if (dupdic.ContainsKey(clone.val))
+                    {
+                        if (dupdic[clone.val] != 0)
+                        {
+                            dupdic[clone.val]++;
+                            clone = clone.next;
+                        }
+                        else
+                        {
+                            dupdic[clone.val]++;
+                            sd.Add(clone.val, clone);
+                            break;
+                        }
+                        
+                    }
+                    else
+                    {
+                        dupdic.Add(clone.val, 1);
+                        sd.Add(clone.val, clone);
+                        break;
+                    }
+                }
+                
+
+            }
+
+            return ret.next;
+        }
+    }
+}
