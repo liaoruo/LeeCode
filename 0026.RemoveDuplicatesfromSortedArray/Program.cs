b9f88e4bdc6a13605434732e4bba72a19923145a
liaoruo <chenxin.private@gmai.com>
2015-05-26 22:17:11 +0800

    Problem0026

---
+++ 0026.RemoveDuplicatesfromSortedArray/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0026.RemoveDuplicatesfromSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1,1,2,2,3,3,4,4,5,5,5,5};
+            Solution solution = new Solution();
+            var ret = solution.RemoveDuplicates(nums);
+            System.Console.WriteLine(ret);
+            for (var i = 0; i < ret; i++)
+                System.Console.Write(nums[i]);
+        }
+        public class Solution
+        {
+            public int RemoveDuplicates(int[] nums)
+            {
+                if (nums == null)
+                    return 0;
+
+                var n = nums.Count();
+                if (n <= 1)
+                    return n;
+
+                var dup = 0;
+                for (int i = 0, j = 0; i < n; i++)
+                {
+                    nums[i] = nums[j];
+                    var k = 1;
+                    while (j + k < n && nums[j + k] == nums[j])
+                    {
+                        k++;
+                        dup++;
+                    }
+                    
+                    if (j + k == n)
+                    {
+                        break;
+                    }
+                    j += k;
+                }
+                return n - dup;
+            }
+        }
+    }
+}
