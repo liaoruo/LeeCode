9ed0799efe2822f5b191221bf97e3354b69dcd06
liaoruo <chenxin.private@gmail.com>
2015-07-13 00:21:30 +0800

    Problem0041

---
+++ 0041.FirstMissingPositive/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0041.FirstMissingPositive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            var ret = solution.FirstMissingPositive(new int[] { });
+            System.Console.WriteLine(ret);
+        }
+    }
+    public class Solution
+    {
+        public int FirstMissingPositive(int[] nums)
+        {
+            HashSet<int> hs = new HashSet<int>();
+            foreach (var i in nums)
+            {
+                hs.Add(i);
+            }
+            for (var i = 1; i <= nums.Length + 1; i++)
+            {
+                if (!hs.Contains(i))
+                {
+                    return i;
+                }
+            }
+            return 1;
+        }
+    }
+}
